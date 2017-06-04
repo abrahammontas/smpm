48b6cc6768228ce66b8fcfa12d8491458bbed9dd
Abraham Montas <abrahammontas@gmail.com>
2017-06-04 11:41:39 +0200

    remove fields from accounts table

--- database/migrations/2017_06_02_171536_remove_fields_from_accout_user_socialprovidertable.php
+++ database/migrations/2017_06_02_171536_remove_fields_from_accout_user_socialprovidertable.php
@@ -13,7 +13,14 @@ class RemoveFieldsFromAccoutUserSocialprovidertable extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+            $table->dropColumn('provider');
+            $table->dropColumn('provider_id');
+            $table->dropColumn('password');
+            $table->integer('social_provider_id')->unsigned();
+            $table->foreign('social_provider_id')->references('id')->on('social_providers');
+        });
     }
 
     /**
@@ -23,6 +30,13 @@ class RemoveFieldsFromAccoutUserSocialprovidertable extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned();
+            $table->string('provider');
+            $table->string('provider_id');
+            $table->string('password');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->dropColumn('social_provider_id');
+        });
     }
 }
